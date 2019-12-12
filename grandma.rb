d26a56904ede398113edf126c030f5baa07f33ae
Austin Johnson <technicallyaustin@gmail.com>
2019-12-12 03:30:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,14 @@
 # Write a speak_to_grandma method.
- 
+ def speak_to_grandma(phrase = "string")
+   if phrase == "I LOVE YOU GRANDMA!"
+     puts "I LOVE YOU TOO PUMPKIN!"
+   elsif phrase == phrase.upcase
+   puts "NO, NOT SINCE 1938!"
+ else puts "HUH?! SPEAK UP SONNY!"
+ end
+ end
+     
+   
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
